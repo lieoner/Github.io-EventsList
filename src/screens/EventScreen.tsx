02562c97b4e0fc40b8a5a377e649c3daab6ae13b
lieoner <popcaric@gmail.com>
2021-08-28 08:43:51 +0700

    check unkonwn props of Events

--- src/screens/EventScreen.tsx
+++ src/screens/EventScreen.tsx
@@ -16,9 +16,17 @@ export const EventScreen: React.FC<{}> = () => {
             <ScrollView>
                 <Text>{`id: ${event.id}`}</Text>
                 <Text>{`type: ${event.type}`}</Text>
-                <Text>{`actor: ${JSON.stringify(event.actor, null, 2)}`}</Text>
-                <Text>{`repo: ${JSON.stringify(event.repo, null, 2)}`}</Text>
-                <Text>{`payload: ${JSON.stringify(event.payload, null, 2)}`}</Text>
+                {event.actor ? (
+                    <Text>{`actor: ${JSON.stringify(event.actor, null, 2)}`}</Text>
+                ) : (
+                    <></>
+                )}
+                {event.repo ? <Text>{`repo: ${JSON.stringify(event.repo, null, 2)}`}</Text> : <></>}
+                {event.payload ? (
+                    <Text>{`payload: ${JSON.stringify(event.payload, null, 2)}`}</Text>
+                ) : (
+                    <></>
+                )}
                 <Text>{`public: ${event.public.toString()}`}</Text>
                 <Text>{`created_at: ${event.created_at}`}</Text>
                 {event.org ? <Text>{`org: ${JSON.stringify(event.org, null, 2)}`}</Text> : <></>}
